b667716c7535a1afadc378ede5b81267ba728d85
Omar Yei Gaston Chalas <ogaston@adm.unapec.edu.do>
2020-02-24 10:57:07 -0400

    final solution (simplifying the iteration number | seconds)

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function bomberMan(n, grid) {
   var redo = true; // referencia de repeticion de procesos apartir del 4to segundo
-  var memoArr = [];
+  var memoArr = []; // Arreglo utilizado para memorizar el nuevo arreglo luego post-detonación
 
   /**
-   * Memoriza en la variable 'memo' las posiciones que serán afectadas por las bombas actualmente ubicadas
+   * Utiliza las coordenadas para validar y retornar que estado debera cambiar en dicha posición
    *
    * @param {int} colum índice de la columna
    * @param {Array} columArr arreglo completo de la columna
@@ -30,35 +30,27 @@ function bomberMan(n, grid) {
    */
   function fillEmptyWithBombs(positionsArray) {
     memoArr = [];
-    var columCreated = [];
-    var rowCreated = [];
+    let columCreated = [];
 
-    for (let columIndex = 0; columIndex < positionsArray.length; columIndex++) {
+    for (let colum = 0; colum < positionsArray.length; colum++) {
+      let rowCreated = [];
       let columArr = positionsArray;
-      let rowArray = positionsArray[columIndex];
+      let rowArray = positionsArray[colum];
       memoArr.push([]);
 
-      for (let rowIndex = 0; rowIndex < rowArray.length; rowIndex++) {
-        memoArr[columIndex].push(
-          getPosState(columIndex, columArr, rowIndex, rowArray)
-        );
+      for (let row = 0; row < rowArray.length; row++) {
+        memoArr[colum].push(getPosState(colum, columArr, row, rowArray));
         rowCreated.push("O");
       }
+
       columCreated.push(rowCreated);
     }
 
     return columCreated;
-    /*     return positionsArray.map((rowArray, colum, columArr) => {
-      memoArr.push([]);
-      return rowArray.map((state, row, rowArr) => {
-        memoArr[colum].push(getPosState(colum, columArr, row, rowArr));
-        return "O";
-      });
-    }); */
   }
 
   /**
-   * Genera el efecto de detonar las bombas utilizando las posiciones memorizadas por la variable 'memo' y reinicializandola.
+   * Genera el efecto de detonar las bombas utilizando las posiciones memorizadas por la variable 'memoArr'
    *
    */
   function detonateBombs() {
@@ -88,9 +80,11 @@ function bomberMan(n, grid) {
     return detonateBombs(result);
   }
 
+  if (n == 0 || n == 1) return grid; // devuelve el mismo grid en caso de que los segundos sean 1 o 0
+
   let result = getPositionArray(grid); // convierte la variable grid en una matriz.
 
-  for (let i = 1; i <= n; i++) {
+  for (let i = 1; i <= 4 + (n % 4); i++) {
     if (i == 1) result = result; // el primer segundo las posiciones se mantienen intactas
     if (i == 2) result = fillEmptyWithBombs(result); // el segundo segundo llena las posiciones vacias con bombas
     if (i == 3) result = detonateBombs(result); // el tercer segundo explotan las primeras bombas insertadas
